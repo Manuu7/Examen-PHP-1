2a751fb5e6d5171e83a01324865b6bba9c171dbc
“manuel” <manusfdk7@gmail.com>
2021-11-22 10:08:46 +0100

    commit ejercicio 1 y 2

---
+++ Examen.php
@@ -0,0 +1,78 @@
+<?php
+
+#Variables metodo POST
+$numrecog1=$_POST["operando1"];
+$numrecog2=$_POST["operando2"];
+$selector=$_POST["operador"];
+
+
+#Funcion Decimal a binario.
+function debi($numrecog1){
+    $res=decbin($numrecog1);
+    echo "El número " . $numrecog1 . " es " . $res . " en binario";
+}
+
+
+#Funcion Decimal a octal.
+function deoc($numrecog1){
+    $res= decoct($numrecog1);
+    echo "El número " . $numrecog1 . " es " . $res . " en octal";
+}
+
+#Funcion Decimal a hexadecimal.
+function dehe($numrecog1){
+    $res= dechex($numrecog1);
+    echo "El número " . $numrecog1 . " es " . $res . " en hexadecimal";
+}
+
+#Funcion Binario a decimal.
+function bide($numrecog1){
+    $res= bindec($numrecog1);
+    echo "El número " . $numrecog1 . " es " . $res . " en decimal";
+}
+
+#Funcion Octal a deciaml.
+function ocde($numrecog1){
+    $res= octdec($numrecog1);
+    echo "El número " . $numrecog1 . " es " . $res . " en decimal";
+}
+
+#Funcion Hexadecimal a decimal.
+function hede($numrecog1){
+    $res= hexdec($numrecog1);
+    echo "El número " . $numrecog1 . " es " . $res . " en decimal";
+}
+
+
+#Realizacion de variables
+switch ($selector) {
+    case 'Decbin': 
+        debi($numrecog1);
+    break;
+    
+    case 'Decoct': 
+        deoc($numrecog1);
+    break;
+    
+    case 'Dechex': 
+        dehe($numrecog1);
+    break;
+
+    case 'Bindec': 
+        bide($numrecog1);
+    break;
+
+    case 'Ocdec': 
+        ocde($numrecog1);
+    break;
+
+    case 'Hexdec': 
+        hede($numrecog1);
+    break;
+
+}
+
+
+?>
+</body>
+</html>
---
+++ Examen2.php
@@ -0,0 +1,74 @@
+<?php
+
+$text1=$_POST["texto1"];
+$text2=$_POST["texto2"];
+$text3=$_POST["texto3"];
+$selector=$_POST["texto"];
+
+
+
+#Texto en mayusculas.
+function mayusculas($text1){
+    $res= strtoupper($text1);
+    print "En mayusculas es: ".$res."<br>";
+}
+
+
+#Desordenar.
+function desordenar($text1){
+
+}
+#Convertir en array.
+function convertir($text1){
+    $array1 = str_split($text1);
+    
+    print_r($array1);
+    
+}
+#Contar.
+function cuentaletras($text1){
+
+    $res=strlen($text1);
+
+    print "El resultado es ".$res."<br>";
+
+}
+#Reemplazar.
+function reemplazar($text3,$text2,$text1){
+    $res=str_replace($text3,$text2,$text1);
+    print "Ahora es: ".$res."<br>";
+
+}
+
+
+
+switch ($selector) {
+    case 'mayus':
+       mayusculas($text1);
+    break;
+    case 'desord':
+        desordenar($text2);
+    break;
+    case 'convert':
+        convertir($text1,$text2,$text3);
+    break;
+    case 'contar':
+        cuentaletras($text1);
+    break;
+    case 'reemp':
+        reemplazar($text3,$text2,$text1);
+    break;
+    default:
+        
+    break;
+}
+
+
+
+
+
+
+?>
+
+</body>
+</html>
